91af2534d04cb295597696267efabe1659d92cdb
6410110429 <6410110429@psu.ac.th>
2023-03-06 11:07:56 +0700

    Notice when leave chat and romove user from Chat

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 console.log(process.env.HARPERDB_URL); // remove this after you've confirmed it working
 const harperSaveMessage = require('./services/harper-save-message');
 const harperGetMessages = require('./services/harper-get-messages');
+const leaveRoom = require('./utils/leave-room'); // Add this
+
 const express = require('express');
 const app = express();
 http = require('http');
@@ -63,6 +65,31 @@ io.on('connection', (socket) => {
         harperSaveMessage(message, username, room, __createdtime__) // Save message in db
           .then((response) => console.log(response))
           .catch((err) => console.log(err));
+      });
+      socket.on('leave_room', (data) => {
+        const { username, room } = data;
+        socket.leave(room);
+        const __createdtime__ = Date.now();
+        // Remove user from memory
+        allUsers = leaveRoom(socket.id, allUsers);
+        socket.to(room).emit('chatroom_users', allUsers);
+        socket.to(room).emit('receive_message', {
+          username: CHAT_BOT,
+          message: `${username} has left the chat`,
+          __createdtime__,
+        });
+        console.log(`${username} has left the chat`);
+      });
+      socket.on('disconnect', () => {
+        console.log('User disconnected from the chat');
+        const user = allUsers.find((user) => user.id == socket.id);
+        if (user?.username) {
+          allUsers = leaveRoom(socket.id, allUsers);
+          socket.to(chatRoom).emit('chatroom_users', allUsers);
+          socket.to(chatRoom).emit('receive_message', {
+            message: `${user.username} has disconnected from the chat.`,
+          });
+        }
       });
 });
 
